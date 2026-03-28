0ddca0130d5629785160edd4e3d03e417f317876
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-28 10:48:15 +0530

    Create Staff entity and repository

---
+++ backend/src/main/java/com/saloon/repository/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.saloon.repository;
+
+import com.saloon.model.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+}
