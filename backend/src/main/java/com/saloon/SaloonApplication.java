e3c3b3743be699131c97adebe7d815db81175e6f
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-28 10:26:38 +0530

    Implement JWT authentication APIs

--- backend/src/main/java/com/saloon/SaloonApplication.java
+++ backend/src/main/java/com/saloon/SaloonApplication.java
@@ -1,11 +1,27 @@
 package com.saloon;
 
+import com.saloon.model.Role;
+import com.saloon.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SaloonApplication {
     public static void main(String[] args) {
         SpringApplication.run(SaloonApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+                roleRepository.save(new Role(null, "ROLE_USER"));
+            }
+            if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+                roleRepository.save(new Role(null, "ROLE_ADMIN"));
+            }
+        };
     }
 }
