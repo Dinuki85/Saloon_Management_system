9ebe3dd03db35c21ac9744e773f8fb562979fe26
Dinuki85 <prarthanadinuki2@gmail.com>
2026-03-28 10:22:41 +0530

    Create User and Role entities

---
+++ backend/src/main/java/com/saloon/model/User.java
@@ -0,0 +1,38 @@
+package com.saloon.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", 
+    uniqueConstraints = { 
+        @UniqueConstraint(columnNames = "email") 
+    })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", 
+        joinColumns = @JoinColumn(name = "user_id"), 
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
